41453ba1b1de3bad5aa1e3462c8b0de7db770436
Yi-Ta <f8162426@hotmail.com>
2024-04-10 20:53:33 -0700

    Added Activity 3, added an array and object, and showed it in console

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Activity 1
 // alert("Hello World");
 
+// Activity 2
 const name = "Adam";
 const age = 30;
 const hasCats = true;
@@ -12,3 +14,33 @@ console.log(name + " is " + age + " years old");
 console.log(name + " has cats: " + hasCats);
 console.log(name + "'s dream job is " + dream);
 console.log(name + "'s current job is " + job);
+
+// Activity 3
+let todoList = [
+  "Drink coffee in the afternoon.",
+  "Do the daily mission of games on my cell phone.",
+  "Watch unseen animes and movies",
+  "Have a greate buffet dinner.",
+];
+
+let todoListCount = todoList.length;
+
+let myThermosFlask = {
+  objectName: "Thermos Flask",
+  color: "white",
+  size: "medium",
+  material: "stainless steel",
+  isInWarranty: true,
+  price: 150,
+};
+
+console.log("There are " + todoList.length + " things to do.");
+console.log("The last thing to do is: " + todoList[todoList.length - 1]);
+
+console.log(
+  "And I will carry my " +
+    myThermosFlask.material +
+    " " +
+    myThermosFlask.objectName +
+    " with me."
+);
