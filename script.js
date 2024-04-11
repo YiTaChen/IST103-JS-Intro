ff1c9198990f0b5c62b456022002a0c4fb1f28fb
Yi-Ta <f8162426@hotmail.com>
2024-04-10 19:34:03 -0700

    added console.log with varibles

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ const hasCats = true;
 
 let dream;
 let job = null;
+
+console.log("This person's name is " + name);
+console.log(name + " is " + age + " years old");
+console.log(name + " has cats: " + hasCats);
+console.log(name + "'s dream job is " + dream);
+console.log(name + "'s current job is " + job);
